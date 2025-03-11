5605f246b45802a4a63202534d79a07bfebb2c9f
Kevin Rivera <kevinrev26@gmail.com>
2025-03-10 22:07:36 -0600

    Analytics html scaffolding added

--- shop/urls.py
+++ shop/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.show, name='shop.show'),
+    path('analytics', views.show_analytics, name='shop.analytics'),
 ]
