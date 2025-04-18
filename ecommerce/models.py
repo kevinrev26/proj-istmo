efc6b9ccc5be171028027a3089cba18cff69dc70
kevinrev26 <kevinrev26@gmail.com>
2025-04-18 13:15:48 -0600

    Category model added, seed for categories and subcategories also added

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -2,18 +2,37 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    parent =  models.ForeignKey(
+        'self', on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='subcategories'
+    )
+    
+    def __str__(self):
+        return self.name
+    
+    def is_main_category(self):
+        return self.parent is None
+
+
 class Product(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     price = models.FloatField()
     description = models.TextField()
     image = models.ImageField(upload_to='product_images/')
-
+    subcategory = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+    )
 
     def __str__(self):
         return str(self.id) + ' - ' + self.name
     
-
 class Review(models.Model):
     id = models.AutoField(primary_key=True)
     comment = models.CharField(max_length=255)
