892798150b7af5705f030ad862780561e01d411a
Kevin Rivera <kevinrev26@gmail.com>
2025-08-18 16:59:20 -0600

    [86b67f2n0] FR1.3.2: Password reset functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,6 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
+from django.contrib.auth import views as auth_views
+
 from . import views
 
 urlpatterns = [
@@ -7,4 +9,28 @@ urlpatterns = [
     path('logout/', views.logout, name='accounts.logout'),
     path('orders/', views.orders, name='accounts.orders'),
     path('activate/<uidb64>/<token>/', views.activate_account, name='accounts.activate'),
+    # Password reset URLs
+    path('password_reset/', 
+         auth_views.PasswordResetView.as_view(
+             template_name='accounts/password_reset.html',
+             email_template_name='accounts/password_reset_email.html',
+             subject_template_name='accounts/password_reset_subject.txt'
+         ), 
+         name='password_reset'),
+    path('password_reset/done/', 
+         auth_views.PasswordResetDoneView.as_view(
+             template_name='accounts/password_reset_done.html'
+         ), 
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', 
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='accounts/password_reset_confirm.html',
+             success_url=reverse_lazy('password_reset_complete')
+         ), 
+         name='password_reset_confirm'),
+    path('reset/done/', 
+         auth_views.PasswordResetCompleteView.as_view(
+             template_name='accounts/password_reset_complete.html'
+         ), 
+         name='password_reset_complete'),
 ]
